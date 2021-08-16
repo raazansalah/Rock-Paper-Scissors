faf406fb037cd70189308881e63b5aa787b66cfb
raazansalah <razansalah022@gmail.com>
2021-08-16 14:21:37 +0200

    done

--- script.js
+++ script.js
@@ -4,24 +4,35 @@ let roundWinner = ''
 let playerScore = 0
 let computerScore = 0
 let Winner = ''
-let computerSelection = computerPlay(playValues);
-let player_selection = playerSelection();
-//////////////// random Selection from array to computer
+let computerSelection = '';
+let player_selection = ''
+//DOM//
+const Winer = document.getElementById('winner');
+const score = document.getElementById('scoretag');
+const userButtons = document.querySelectorAll('.playerbtn')
+
+restartGame = () => location.reload();
+// random Selection from array to computer
 function computerPlay(array) {
     let randomNumber = Math.floor(Math.random() * playValues.length);
     return array[randomNumber];
 
 }
-//////////User Select 
-function playerSelection() {
-    let player = prompt("choose Rock or Paper or Scissors");
-    return player;
+//User Select 
+function getPlayerSelection(btnId) {
+    player_selection = playValues[btnId]
+    computerSelection = computerPlay(playValues);
+    console.log(player_selection, computerSelection)
+    playRound(player_selection, computerSelection);
+    //playFiveRounds();
+
 }
 
-//////////////// one Round Game
+//one Round Game
 function playRound(playerselection, computerselection) {
+
     if (computerselection == playerselection) {
-        roundWinner = "tie Game" ;
+        roundWinner = "Tie Game";
     }
     if (
 
@@ -29,9 +40,9 @@ function playRound(playerselection, computerselection) {
         (playerselection === 'Scissors' && computerselection === 'Paper') ||
         (playerselection === 'Paper' && computerselection === 'Rock')
 
-    ){
+    ) {
         playerScore++
-        roundWinner ="player";
+        roundWinner = "You Win WOHOOO";
     }
     if (
 
@@ -39,15 +50,15 @@ function playRound(playerselection, computerselection) {
         (computerselection === 'Scissors' && playerselection === 'Paper') ||
         (computerselection === 'Paper' && playerselection === 'Rock')
 
-    ){
+    ) {
         computerScore++
-        roundWinner = "computer";
+        roundWinner = "You Lost !";
     }
-    printResult(roundWinner , playerScore , computerScore);
-
+    printResult(roundWinner, playerScore, computerScore);
 }
+
 // Five Rounds and Print the Winner
-function playFiveRounds (){
+/* function playFiveRounds (){
     for(i=0 ;i<5 ;i++){
         computerSelection = computerPlay(playValues);
         player_selection = playerSelection();
@@ -64,13 +75,21 @@ function playFiveRounds (){
         Winner ="Computer"
     }
     console.log(Winner , playerScore , computerScore)
+} */
+
+
+function printResult(roundwinner, playerScore, computerScore) {
+    score.innerHTML = 'Player'+ ':'+ playerScore + ':' + 'Computer'+ ':'+ computerScore;
+    Winer.innerHTML = roundWinner;
+    if (playerScore == 5 || computerScore == 5) {
+        for (let i = 0; i < userButtons.length; i++) {
+            userButtons[i].disabled = true;
+        }
+    }
 }
 
-function printResult(roundwinner , playerScore , computerScore)  {
-    console.log(roundwinner , playerScore,computerScore);
-}
 
-console.log(player_selection ,computerSelection);
 
-//playRound(player_selection,computerSelection);
-playFiveRounds();
+
+
+
